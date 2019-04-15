4c36b56fd1527b164673e6ea04733a19bb7652cf
MarcusAronsson1 <marcus.dag.aronsson@gmail.com>
2019-04-15 14:10:18 +0200

    Android App Bluetooth Activity
    Created a new activity to have a start-screen in the app where you
    establish the bluetooth connection.

---
+++ android_app/app/src/main/java/org/miniproject/safesweeper/BluetoothActivity.java
@@ -0,0 +1,13 @@
+package org.miniproject.safesweeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BluetoothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+    }
+}
