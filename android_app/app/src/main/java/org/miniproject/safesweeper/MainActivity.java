17660fe774d7bee6c29bb465de58e6c3a50671c4
MarcusAronsson1 <marcus.dag.aronsson@gmail.com>
2019-04-30 11:02:24 +0200

    made code more clear by adding constants

--- android_app/app/src/main/java/org/miniproject/safesweeper/MainActivity.java
+++ android_app/app/src/main/java/org/miniproject/safesweeper/MainActivity.java
@@ -22,6 +22,7 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
+    Button getLocationBtn;
     TextView textView1;
     SeekBar throttleBar;
     SeekBar steeringBar;
@@ -35,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int STEERING_MIN = -50;
     public static final int STEERING_DEFAULT = 50;
+
+    //Car commands
+    public static final String MOVE_FORWARD = "0";
+    public static final String STAND_STILL = "4";
+    public static final String MOVE_BACKWARD = "1";
+    public static final String AUTOMATIC_MODE = "5";
+    public static final String MANUAL_MODE = "6";
+    public static final String STEER_RIGHT = "3";
+    public static final String STEER_LEFT = "2";
 
     private int speedValue;
     private int steerValue;
@@ -63,12 +73,9 @@ public class MainActivity extends AppCompatActivity {
         steeringText = (TextView) findViewById(R.id.steeringText);
         connectionTextView = (TextView) findViewById(R.id.connectionTextView);
         steeringToggle = (ToggleButton) findViewById(R.id.steeringToggle);
+        //TODO initialize getLocationBtn and the "mine notification" element.
 
         new ConnectBT().execute();
-
-        //toggle switch to go between manual steering and automatic steering, starts with manual
-
-
 
     }
 
@@ -135,21 +142,21 @@ public class MainActivity extends AppCompatActivity {
                         throttleText.setText(speedValue + "");
 
                         if (speedValue > 25){ //go forward when seekbar is above 25%
-                            command = "0";
+                            command = MOVE_FORWARD;
                             try {
                                 outputStream.write(command.getBytes());
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         } else if (speedValue == 0){ //stand still
-                            command = "4";
+                            command = STAND_STILL;
                             try {
                                 outputStream.write(command.getBytes());
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }else if (speedValue < 0){ //go backwards
-                            command = "1";
+                            command = MOVE_BACKWARD;
                             try {
                                 outputStream.write(command.getBytes());
                             } catch (IOException e) {
@@ -173,13 +180,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+                //toggle switch to go between manual steering and automatic steering, starts with manual
                 steeringToggle.setOnClickListener(new View.OnClickListener() { //Toggle between automatic and manual via different commands
                     @Override
                     public void onClick(View v) {
                         if(steeringToggle.isChecked()){
                             textView1.setText("Automatic");
                             //auto();
-                            command = "5";
+                            command = AUTOMATIC_MODE;
                             try {
                                 outputStream.write(command.getBytes());
                             } catch (IOException e) {
@@ -189,7 +197,7 @@ public class MainActivity extends AppCompatActivity {
 
                         }else
                             textView1.setText("Manual");
-                        command = "6";
+                        command = MANUAL_MODE;
                         try {
                             outputStream.write(command.getBytes());
                         } catch (IOException e) {
@@ -206,21 +214,21 @@ public class MainActivity extends AppCompatActivity {
                         throttleText.setText(steerValue + "");
 
                         if (steerValue > 0){ //go right
-                            command = "3";
+                            command = STEER_RIGHT;
                             try {
                                 outputStream.write(command.getBytes());
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }else if (steerValue < 0){ //go left
-                            command = "2";
+                            command = STEER_LEFT;
                             try {
                                 outputStream.write(command.getBytes());
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }else if (steerValue == 0){ //stop
-                            command = "4";
+                            command = STAND_STILL;
                             try {
                                 outputStream.write(command.getBytes());
                             } catch (IOException e) {
