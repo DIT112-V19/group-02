1370683f7205023c6315ebdbe54b75db2db30b89
August Axelsson <43994417+AugustAxelsson@users.noreply.github.com>
2019-05-30 17:18:11 +0200

    Delete connection.php

--- php/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server_name = "mysql681.loopia.se";
-$db_name = "axronstudio_nu_db_3";
-$mysql_username = "minee@a254864";
-$mysql_password = "dinmamma123";
-
-$connection = new mysqli($server_name, $mysql_username, $mysql_password, $db_name);
-?>
